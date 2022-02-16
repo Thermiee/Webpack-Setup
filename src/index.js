95ab5c09588cf8e32dc88485e80013b17ae46cdb
Thermiee <navyblack16@yahoo.com>
2022-02-16 09:04:01 +0100

    fix linters error

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import _ from 'lodash';
 import printMe from './print.js';
 import './style.css';
 
-
 function component() {
   const element = document.createElement('div');
   const btn = document.createElement('button');
